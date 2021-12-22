9e6a8ce22ef57c856c8dc997ace35b52b1e50ecd
ydlong <donlongca@gmail.com>
2021-12-22 00:15:33 -0500

    changes

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -19,24 +19,13 @@ export class ScoreComponent implements OnInit {
     this.scores = this.dataService.getScores();
   }
 
-
-  round_id: number = 0 ;
-  game_id: number = 0;
-  p1_m: number = 0;
-  p2_m: number = 0;
-  p3_m: number = 0;
-  p4_m: number = 0;
-
   displayedColumns: string[] = [];
-  columnsToDisplay: string[] = [];
-  //displayedColumns: string[] = ['round_id','game_id', 'p1_m', 'p2_m', 'p3_m', 'p4_m'];  
   dataSource = this.scores;
 
   ngOnInit(): void {
     this.getScores();
     this.displayedColumns = this.dataService.getColScore();
-    this.columnsToDisplay = this.displayedColumns.slice();
-    console.warn(this.scores, this.displayedColumns, this.columnsToDisplay);
+    console.warn(this.scores, this.displayedColumns);
   }
 
 }
