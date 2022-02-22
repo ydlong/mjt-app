9b098b20ff13782e23682146215ff2cf9e09bf83
ydlong <donlongca@gmail.com>
2022-02-22 18:03:05 -0500

    changes

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -4,12 +4,8 @@ import { Score } from '../score';
 import { MatTable } from '@angular/material/table';
 import { AppService } from '../app.services';
 import { Subscription } from 'rxjs';
-<<<<<<< HEAD
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
-=======
-import { FormArray, FormGroup, FormBuilder } from '@angular/forms';
-import { ThrowStmt } from '@angular/compiler';
->>>>>>> e8d7bdd574eeaaa55cddd73b1ed215beb1aae66e
+
 
 @Component({
   selector: 'app-score',
@@ -19,12 +15,12 @@ import { ThrowStmt } from '@angular/compiler';
 
 export class ScoreComponent implements OnInit, AfterViewInit {
 
-  public mtformgroup!: FormGroup;
-  public mtformarray!: FormArray;
+  //public mtformgroup!: FormGroup;
+  //public mtformarray!: FormArray;
 
   clickEventsubscription:Subscription;
 
-  constructor( private dataService: DataService, private appService:AppService, private fb:FormBuilder){
+  constructor( private dataService: DataService, private appService:AppService){
     this.clickEventsubscription = this.appService.getScoreChangeEvent().subscribe(()=>{
       this.getScores();
       this.dataSource = this.scores;
@@ -59,11 +55,8 @@ export class ScoreComponent implements OnInit, AfterViewInit {
     this.displayedColumns = this.dataService.getColScore();
     this.tblDef = this.dataService.getTableDef();  
     this.colHeaderToDisplayed = this.displayedColumns.slice();
-<<<<<<< HEAD
-    this.mtformarray = this.fb.array([]);
-    this.mtformgroup = this.fb.group({FormArray: this.mtformarray});
-=======
->>>>>>> e8d7bdd574eeaaa55cddd73b1ed215beb1aae66e
+    //this.mtformarray = this.fb.array([]);
+    //this.mtformgroup = this.fb.group({FormArray: this.mtformarray});
     
   }
 
